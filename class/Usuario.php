268ec44bfb9a6bb815ccdc7997c46f22fbf78922
Carlos Silva <carfsilva@gmail.com>
2018-07-09 19:14:10 +0100

    dao

--- class/Usuario.php
+++ class/Usuario.php
@@ -52,8 +52,8 @@ class Usuario {
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :id", array(":id" => $id));
 
         if (count($results) > 0):
-
-            $row = $results[0];
+            // Array que é retornado (array de arrays)
+            $row = $results[0]; 
 
             $this->setIdusuario($row['idusuario']);
             $this->setDeslogin($row['deslogin']);
@@ -63,6 +63,48 @@ class Usuario {
         endif;
     }
 
+    public static function getList()
+    {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+
+    public static function search($login)
+    {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :login ORDER BY deslogin", array(
+            ":login" => "%$login%"
+        ));
+    }
+
+    public function login($user, $password)
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :user AND dessenha = :password", array(
+            ":user" => $user, 
+            ":password" => $password
+        ));
+
+        if (count($results) > 0):
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        else:
+
+            throw new Exception("Utilizador ou password errados.");
+
+        endif;
+    }
+
+    // Para um único registo
     public function __toString()
     {
         return json_encode(array(
--- index.php
+++ index.php
@@ -2,10 +2,26 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Careega um usuário
+/* $root = new Usuario();
 
 $root->loadById(3);
 
 // Carrega o obecto através do método __toString()
-echo $root;
+echo $root; */
+
+// Carrega uma lista de usuários
+// $usuarios = Usuario::getList();
+
+// echo json_encode($usuarios);
+
+// Carrega uma lista de usuários por login
+// $lista = Usuario::search("carl");
+// echo json_encode($lista);
+
+// Carregar usuario com nome e password
+$utilizador = new Usuario();
+$utilizador->login("carl thomas", "carl");
+
+echo $utilizador;
 ?>
